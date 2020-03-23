f2bf1ea229d465340d061e273f6e8d643680596c
Ani Jekova <anjekova@gmail.com>
2020-03-23 11:56:56 +0200

    bookings added

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,21 +1,23 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('./../models/tourModel');
+const Booking = require('./../models/bookingModel');
 const catchAsync = require('./../utils/catchAsync');
-// const factory = require('./handlerFactory');
-// const AppError = require('./../utils/appError');
+const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // get the currently booked  tour
   const tour = await Tour.findById(req.params.tourId);
-  console.log(tour);
 
   // create the checkout checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    // not secure
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
-    client_reference_id: req.params.tourID,
+    client_reference_id: req.params.tourId,
     line_items: [
       {
         name: `${tour.name} Tour`,
@@ -35,3 +37,24 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     session
   });
 });
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // temporary , not secure , everyone can make bookings without paying
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  return res.redirect(req.originalUrl.split('?')[0]);
+});
+
+exports.getAllBookings = factory.getAll(Booking);
+
+exports.getBooking = factory.getOne(Booking);
+
+exports.createBooking = factory.createOne(Booking);
+
+exports.updateBooking = factory.updateOne(Booking);
+
+exports.deleteBooking = factory.deleteOne(Booking);
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
@@ -45,6 +46,21 @@ exports.getAccount = (req, res) => {
     title: 'My account'
   });
 };
+
+exports.getMyTours = catchAsync(async (req, res, next) => {
+  // find all bookings
+  const bookings = await Booking.find({ user: req.user.id });
+  console.log(bookings);
+  // find all tours with the returned ids
+  const tourIds = bookings.map(el => el.tour);
+  console.log(tourIds);
+  const tours = await Tour.find({ _id: { $in: tourIds } });
+
+  res.status(200).render('overview', {
+    title: 'My tours',
+    tours
+  });
+});
 
 exports.updateUserData = catchAsync(async (req, res, next) => {
   const updatedUser = await User.findOneAndUpdate(
